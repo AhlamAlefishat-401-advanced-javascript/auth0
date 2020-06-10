ebb4c07e3bef41713b736f27f326d739b74c0efd
ahlamalefishat96 <60567558+ahlamalefishat96@users.noreply.github.com>
2020-06-10 10:18:50 +0300

    Update auth0.js

--- src/auth0.js
+++ src/auth0.js
@@ -9,7 +9,7 @@ const users = require('./users.js');
 */
 
 // const tokenServerUrl = process.env.TOKEN_SERVER;
-const remoteAPI = 'http://tttauth0.eu.auth0.com/userinfo';
+const remoteAPI = 'https://tttauth0.eu.auth0.com/userinfo';
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const API_SERVER = process.env.API_SERVER;//local
